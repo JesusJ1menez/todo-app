8a4e86c04391ee45778239267c3ea41af4701097
JesusJ1menez <L18091036@zacatepec.tecnm.mx>
2022-12-02 12:46:26 -0600

    Updates

---
+++ precache-manifest.7c77c137460cd259449ee11ee7d4f729.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "056ca2e1a43d91fcd877",
+    "url": "/todo-app/static/css/main.c0e2a3a7.chunk.css"
+  },
+  {
+    "revision": "056ca2e1a43d91fcd877",
+    "url": "/todo-app/static/js/main.056ca2e1.chunk.js"
+  },
+  {
+    "revision": "9ff39862b1de814fb621",
+    "url": "/todo-app/static/js/1.9ff39862.chunk.js"
+  },
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "4fbac5637ccf14625fb986f26dc6b12e",
+    "url": "/todo-app/static/media/emptyToDo.4fbac563.svg"
+  },
+  {
+    "revision": "c486a18092233163a0cb1224535b1a0b",
+    "url": "/todo-app/static/media/noResults.c486a180.svg"
+  },
+  {
+    "revision": "a6ff2ce648ff3421426db7842d40d1d0",
+    "url": "/todo-app/static/media/check.a6ff2ce6.svg"
+  },
+  {
+    "revision": "45d0d71a1f8f92c1da6744303a373163",
+    "url": "/todo-app/static/media/delete.45d0d71a.svg"
+  },
+  {
+    "revision": "d7d0ba135688e4e99c25d4f8b556c7f3",
+    "url": "/todo-app/index.html"
+  }
+];
